5ad2b5256cbf9c55557ef832e71fcd9bce18cd63
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-04 18:47:48 +0100

    footertop with section info background image and image dc in front of background

--- src/components/FooterTop.jsx
+++ src/components/FooterTop.jsx
@@ -1,6 +1,74 @@
 export default function FooterTop() {
     return (
         <>
+            <div className="footer-top text-white pt-5 sfondofootertop position-relative">
+                <div className="container position-relative">
+                    <div className="row">
+
+                        <div className="col-12 col-md-6">
+                            <div className="row">
+                                <div className="col-6 col-md-4">
+                                    <h5>DC COMICS</h5>
+                                    <ul className="list-unstyled">
+                                        <li><a href="" className="text-secondary text-decoration-none">Characters</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Comics</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Movies</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">TV</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Games</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Videos</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">News</a></li>
+                                    </ul>
+                                </div>
+
+                                <div className="col-6 col-md-4">
+                                    <h5>DC</h5>
+                                    <ul className="list-unstyled">
+                                        <li><a href="" className="text-secondary text-decoration-none">Terms Of Use</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Privacy policy (new)</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Ad Choices</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Advertising</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Advertising</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Jobs</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Subscriptions</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Talent Workshops</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">CPSC Certificates</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Ratings</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Shop Help</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Contact Us</a></li>
+                                    </ul>
+                                </div>
+
+                                <div className="col-6 col-md-4">
+                                    <h5>SITES</h5>
+                                    <ul className="list-unstyled">
+                                        <li><a href="" className="text-secondary text-decoration-none">DC</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">MAD Magazine</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">DC Kids</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">DC Universe</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">DC Power Visa</a></li>
+                                    </ul>
+                                </div>
+
+                                <div className="col-6 col-md-4">
+                                    <h5>SHOP</h5>
+                                    <ul className="list-unstyled">
+                                        <li><a href="" className="text-secondary text-decoration-none">Shop DC</a></li>
+                                        <li><a href="" className="text-secondary text-decoration-none">Shop DC Collectibles</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-md-6 position-relative">
+                            <img
+                                className="footer-bg-img position-absolute end-0 top-50 translate-middle-y"
+                                src="/img/dc-logo-bg.png"
+                                alt="DC background"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
