a29c586e03fa867f8061e78f7e7a073548881ebd
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-04 17:07:02 +0100

    main part with background color

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <>
+            <div className="sfondomain text-white py-5" >
+                <h2 className="spostamento-destro"> --&gt; Content goes here &lt;--</h2>
+            </div >
+        </>
+    )
+}
