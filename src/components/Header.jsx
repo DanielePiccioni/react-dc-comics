71e85932667e80a3f1a7d0a1ccb7ceb916d35f7e
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-04 16:42:52 +0100

    header part with img and nav part with some boostrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
 export default function Header() {
     return (
-        <h1>test</h1>
+        <>
+            <div className="container d-flex justify-content-between align-items-center py-3 bg-white">
+                <div className="logo">
+                    <img className="img-fluid" src="./img/dc-logo.png" alt="" />
+                </div>
+                <nav>
+                    <ul className="nav">
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">CHARACTERS</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">COMICS</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">MOVIES</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">TV</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">GAMES</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">COLLECTIBLES</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">VIDEOS</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">FANS</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">NEWS</a></li>
+                        <li className="nav-item"><a className="nav-link text-dark fw-bold" href="">SHOP</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
