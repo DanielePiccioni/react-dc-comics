8da6ab5253ee23e70d82e05197a7ecf76775bf5e
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-05 16:55:25 +0100

    Create a new jsx for jumbotron and connect to main.jsx

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+export default function Jumbotron() {
+    return (
+        <section className="jumbotron">
+            <div className="jumbotron-overlay">
+                <div className="container">
+                    <h2 className="jumbotron-title">CURRENT SERIES</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
+import Jumbotron from "./Jumbotron"
+
 export default function Main() {
     return (
         <>
-            <div className="sfondomain text-white py-5" >
-                <h2 className="spostamento-destro"> --&gt; Content goes here &lt;--</h2>
-            </div >
+            <main>
+                <Jumbotron />
+            </main>
         </>
     )
 }
