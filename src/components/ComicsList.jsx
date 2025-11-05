a6110ca661dbf5e0d3140fb5ca4e448ae537e742
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-05 23:20:34 +0100

    all css from jumbotron and all card and split card 6 and 6 on comicslist

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -3,14 +3,20 @@ import ComicCard from "./ComicCard";
 
 export default function ComicsList() {
     return (
-        <section className="container">
+        <section className="sfondocontainer">
             <div className="row">
-                {comics.map((comic) => (
+                {comics.slice(0, 6).map((comic) => (
                     <ComicCard key={comic.id} comic={comic} />
                 ))}
+            </div>
+            <div className="row">
+                {comics.slice(6, 12).map((comic) => (
+                    <ComicCard key={comic.id} comic={comic} />
+                ))}
+            </div>
+            <div className="load-more-container">
+                <button className="load-more">LOAD MORE</button>
             </div>
         </section>
     )
 }
-
-
