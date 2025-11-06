ff1321e5f0a0bc01eaea60f54c2d553bf2f88f1b
DanielePiccioni <danielepiccioni1@gmail.com>
2025-11-06 19:24:11 +0100

    fix layout with boostrap and css , add component reusable comiccard

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,14 +1,8 @@
-export default function ComicCard({ comic }) {
+export default function ComicCard(props) {
     return (
-        <div className="col">
-            <article className="comic-card">
-                <img
-                    src={comic.thumb}
-                    alt={comic.title}
-                    className="comic-thumb"
-                />
-                <h6 className="comic-series">{comic.series}</h6>
-            </article>
+        <div className="comic-card">
+            <img src={props.image} alt={props.title} className="comic-thumb" />
+            <h6 className="comic-series">{props.title}</h6>
         </div>
     )
 }
--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -4,18 +4,24 @@ import ComicCard from "./ComicCard";
 export default function ComicsList() {
     return (
         <section className="sfondocontainer">
-            <div className="row">
-                {comics.slice(0, 6).map((comic) => (
-                    <ComicCard key={comic.id} comic={comic} />
-                ))}
-            </div>
-            <div className="row">
-                {comics.slice(6, 12).map((comic) => (
-                    <ComicCard key={comic.id} comic={comic} />
-                ))}
-            </div>
-            <div className="load-more-container">
-                <button className="load-more">LOAD MORE</button>
+            <div className="container">
+                {/* riga con 6 colonne per riga */}
+                <div className="row row-cols-6 g-4">
+                    {comics.slice(0, 12).map((comic) => (
+                        <div className="col" key={comic.id}>
+                            <ComicCard
+                                image={comic.thumb}
+                                title={comic.series}
+                            />
+                        </div>
+                    ))}
+                </div>
+
+                <div className="d-flex justify-content-center mt-4">
+                    <button className="btn btn-primary text-uppercase py-2">
+                        Load More
+                    </button>
+                </div>
             </div>
         </section>
     )
